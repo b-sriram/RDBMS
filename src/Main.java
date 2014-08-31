411dc2c456043058e66a031b3ab53a9c2156e1e8
Sriram <sriram@vasudhaika.net>
2014-08-31 17:30:55 +0530

    JDBC Connection Testing

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Main {
+	private static final String	USERNAME = "root";
+	private static final String	PASSWORD = "";
+	private static final String	CONN_STRING = 
+			"jdbc:mysql://localhost/explorecalifornia";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			Connection conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+			System.out.println("DB Connected");
+		} catch (SQLException e) {
+			System.out.println("DB Connection failed");
+			System.err.println(e);
+		}
+
+	}
+
+}
