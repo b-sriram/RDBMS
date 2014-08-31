add2f11dc59908eecc1446262a207213cf44a5fc
Sriram <sriram@vasudhaika.net>
2014-08-31 22:03:38 +0530

    HSQL connection testing

---
+++ src/HsqlTest.java
@@ -0,0 +1,38 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class HsqlTest {
+	private static final String	USERNAME = "root";
+	private static final String	PASSWORD = "";
+	private static final String	CONN_STRING = 
+			"jdbc:hsqldb:data/explorecalifornia";
+	public static void main(String[] args) throws SQLException {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;		
+		try {	
+				conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD); 
+				stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+				rs = stmt.executeQuery("SELECT * FROM states");
+			 
+				rs.last();
+				System.out.println("Number of Records: "+ rs.getRow());
+			
+		} catch (SQLException e) {
+			System.out.println("DB Connection failed");
+			System.err.println(e);
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (stmt != null)
+				stmt.close();
+			if (conn != null)
+				conn.close();
+		}
+
+	}
+
+}
