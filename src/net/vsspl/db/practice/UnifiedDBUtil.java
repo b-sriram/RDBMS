f46d72b8fcbc8a29a2ebf0301084d9a10fe5bb28
Sriram <sriram@vasudhaika.net>
2014-08-31 23:08:21 +0530

    Try with Resource implemented for automatic connection closing

--- src/net/vsspl/db/practice/UnifiedDBUtil.java
+++ src/net/vsspl/db/practice/UnifiedDBUtil.java
@@ -1,7 +1,6 @@
 package net.vsspl.db.practice;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,29 +9,21 @@ public class UnifiedDBUtil {
 	
 	
 	public static void main(String[] args) throws SQLException {
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;		
-		try {	
-				conn = DBUtil.getConnection(DBType.HSQL);
-				//conn = DBUtil.getConnection(DBType.MYSQL);
-				stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-				rs = stmt.executeQuery("SELECT * FROM states");
-			 
+				
+		try ( 
+				Connection conn = DBUtil.getConnection(DBType.HSQL);
+				//Connection conn = DBUtil.getConnection(DBType.MYSQL);
+				Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+				ResultSet rs = stmt.executeQuery("SELECT * FROM states");
+			){	
+				
 				rs.last();
 				System.out.println("Number of Records: "+ rs.getRow());
 			
 		} catch (SQLException e) {
 			System.out.println("DB Connection failed");
 			System.err.println(e);
-		} finally {
-			if (rs != null)
-				rs.close();
-			if (stmt != null)
-				stmt.close();
-			if (conn != null)
-				conn.close();
-		}
+		} 
 
 	}
 
